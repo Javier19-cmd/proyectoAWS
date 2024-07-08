2f92d5658bb05baaa1f82b487e22ae77c82bf2a1
Javier Sebastián Valle <63107636+Javier19-cmd@users.noreply.github.com>
2024-07-08 10:18:02 -0600

    adding some changes

--- server2/app.js
+++ server2/app.js
@@ -1,10 +1,17 @@
 // Módulos requeridos
 const express = require('express');
+const fs = require("fs");
 const http = require('http');
+const https = require('https');
 const helmet = require('helmet');
 var compression = require('compression');
 require('dotenv').config();
 const {v4: uuidv4} = require('uuid');
+
+const httpsServerOptions = {
+    hey: fs.readFileSync(process.env.KEY_PATH),
+    crt: fs.readFileSync(process.env.CRT_PATH)
+}
 
 const app = express();
 app.use(helmet()); // Ayuda a proteger aplicaciones Express
@@ -14,6 +21,14 @@ app.use(compression());
 // Servidor HTTP
 const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
+
+
+const serverHttps = https.createServer(httpsServerOptions, app);
+serverHttps.listen(process.env.HTTPS_PORT, process.env.IP);
+
+app.use((req, res, next) => {
+    if(req.secure) next(); else res.redirect(`https://${req.headers.host}${req.url}`)
+})
 
 // Contenido estático
 app.use(express.static('./public'));
